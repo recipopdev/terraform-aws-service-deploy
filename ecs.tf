fa8fd8ab0aa1db59e4d8c00a0d4dcb4364060b32
Gordon Adam-Santiago <calxus@protonmail.ch>
2022-04-21 17:10:08 +0100

    Attempting to fix definition

--- ecs.tf
+++ ecs.tf
@@ -34,15 +34,14 @@ resource "aws_ecs_task_definition" "main" {
   memory                   = (var.container.memory + (var.service_discovery.enabled ? 256 : 0))
   execution_role_arn       = aws_iam_role.main.arn
   task_role_arn            = aws_iam_role.main.arn
-  container_definitions    = jsonencode(concat(
-    var.service_discovery.enabled ? [data.template_file.service_discovery_container.rendered] : [],
-    [data.template_file.main_container.rendered]
-  ))
+  container_definitions    = "[ ${var.service_discovery.enabled ? join("", [data.template_file.service_discovery_container.rendered, ","]) : ""} ${data.template_file.main_container.rendered} ]"
   runtime_platform {
     operating_system_family = var.windows_deployment ? "WINDOWS_SERVER_2019_CORE" : "LINUX"
     cpu_architecture        = "X86_64"
   } 
 }
+
+
 
 data "template_file" "service_discovery_container" {
   template = file("${path.module}/container_definition.tpl")
