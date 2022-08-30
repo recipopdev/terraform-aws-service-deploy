65feff60856188845a0b567fdc12021d8e1150b2
Calxus <calxus@protonmail.ch>
2022-08-30 20:54:35 +0100

    Update scaling.tf

--- scaling.tf
+++ scaling.tf
@@ -1,140 +1,36 @@
-resource "aws_cloudwatch_metric_alarm" "cpu_high" {
-  count               = var.scaling.enabled ? 1 : 0
-  alarm_name          = "${var.service}-cpu-high"
-  comparison_operator = "GreaterThanOrEqualToThreshold"
-  evaluation_periods  = var.scaling.scale_up.cpu.evaluation_period
-  metric_name         = "CPUUtilization"
-  namespace           = "AWS/ECS"
-  period              = var.scaling.scale_up.cpu.period
-  statistic           = "Average"
-  threshold           = var.scaling.scale_up.cpu.threshold
-  dimensions = {
-    ClusterName = var.cluster
-    ServiceName = var.service
-  }
-  alarm_actions = [aws_appautoscaling_policy.cpu_scale_up_policy[0].arn]
-}
-
-resource "aws_cloudwatch_metric_alarm" "cpu_low" {
-  count               = var.scaling.enabled ? 1 : 0
-  alarm_name          = "${var.service}-cpu-low"
-  comparison_operator = "LessThanOrEqualToThreshold"
-  evaluation_periods  = var.scaling.scale_down.cpu.evaluation_period
-  metric_name         = "CPUUtilization"
-  namespace           = "AWS/ECS"
-  period              = var.scaling.scale_down.cpu.period
-  statistic           = "Average"
-  threshold           = var.scaling.scale_down.cpu.threshold
-  dimensions = {
-    ClusterName = var.cluster
-    ServiceName = var.service
-  }
-  alarm_actions = [aws_appautoscaling_policy.cpu_scale_down_policy[0].arn]
-}
-
-resource "aws_cloudwatch_metric_alarm" "memory_high" {
-  count               = var.scaling.enabled ? 1 : 0
-  alarm_name          = "${var.service}-memory-high"
-  comparison_operator = "GreaterThanOrEqualToThreshold"
-  evaluation_periods  = var.scaling.scale_up.memory.evaluation_period
-  metric_name         = "MemoryUtilization"
-  namespace           = "AWS/ECS"
-  period              = var.scaling.scale_up.memory.period
-  statistic           = "Average"
-  threshold           = var.scaling.scale_up.memory.threshold
-  dimensions = {
-    ClusterName = var.cluster
-    ServiceName = var.service
-  }
-  alarm_actions = [aws_appautoscaling_policy.memory_scale_up_policy[0].arn]
-}
-
-resource "aws_cloudwatch_metric_alarm" "memory_low" {
-  count               = var.scaling.enabled ? 1 : 0
-  alarm_name          = "${var.service}-memory-low"
-  comparison_operator = "LessThanOrEqualToThreshold"
-  evaluation_periods  = var.scaling.scale_down.memory.evaluation_period
-  metric_name         = "MemoryUtilization"
-  namespace           = "AWS/ECS"
-  period              = var.scaling.scale_down.memory.period
-  statistic           = "Average"
-  threshold           = var.scaling.scale_down.memory.threshold
-  dimensions = {
-    ClusterName = var.cluster
-    ServiceName = var.service
-  }
-  alarm_actions = [aws_appautoscaling_policy.memory_scale_down_policy[0].arn]
-}
-
-resource "aws_appautoscaling_policy" "cpu_scale_up_policy" {
+resource "aws_appautoscaling_policy" "cpu_policy" {
   count              = var.scaling.enabled ? 1 : 0
-  name               = "${var.service}-cpu-scale-up-policy"
+  name               = "${var.service}-cpu-policy"
   depends_on         = [aws_appautoscaling_target.scale_target[0]]
-  service_namespace  = "ecs"
-  resource_id        = "service/${var.cluster}/${var.service}"
-  scalable_dimension = "ecs:service:DesiredCount"
-  step_scaling_policy_configuration {
-    adjustment_type         = "ChangeInCapacity"
-    cooldown                = var.scaling.scale_up.cpu.cooldown
-    metric_aggregation_type = "Average"
-    step_adjustment {
-      metric_interval_lower_bound = 0
-      scaling_adjustment          = 1
+  service_namespace  = aws_appautoscaling_target.scale_target.service_namespace
+  resource_id        = aws_appautoscaling_target.scale_target.resource_id
+  scalable_dimension = aws_appautoscaling_target.scale_target.scalable_dimension
+  policy_type        = "TargetTrackingScaling"
+  target_tracking_scaling_policy_configuration {
+    predefined_metric_specification {
+      predefined_metric_type = "ECSServiceAverageCPUUtilization"
     }
+    target_value       = var.scaling.cpu.threshold
+    scale_in_cooldown  = var.scaling.scale_down.cpu.cooldown
+    scale_out_cooldown = var.scaling.scale_up.cpu.cooldown
   }
 }
 
-resource "aws_appautoscaling_policy" "cpu_scale_down_policy" {
+resource "aws_appautoscaling_policy" "memory_policy" {
   count              = var.scaling.enabled ? 1 : 0
-  name               = "${var.service}-cpu-scale-down-policy"
+  name               = "${var.service}-memory-policy"
   depends_on         = [aws_appautoscaling_target.scale_target[0]]
-  service_namespace  = "ecs"
-  resource_id        = "service/${var.cluster}/${var.service}"
-  scalable_dimension = "ecs:service:DesiredCount"
-  step_scaling_policy_configuration {
-    adjustment_type         = "ChangeInCapacity"
-    cooldown                = var.scaling.scale_down.cpu.cooldown
-    metric_aggregation_type = "Average"
-    step_adjustment {
-      metric_interval_upper_bound = 0
-      scaling_adjustment          = -1
+  service_namespace  = aws_appautoscaling_target.scale_target.service_namespace
+  resource_id        = aws_appautoscaling_target.scale_target.resource_id
+  scalable_dimension = aws_appautoscaling_target.scale_target.scalable_dimension
+  policy_type        = "TargetTrackingScaling"
+  target_tracking_scaling_policy_configuration {
+    predefined_metric_specification {
+      predefined_metric_type = "ECSServiceAverageMemoryUtilization"
     }
-  }
-}
-
-resource "aws_appautoscaling_policy" "memory_scale_up_policy" {
-  count              = var.scaling.enabled ? 1 : 0
-  name               = "${var.service}-memory-scale-up-policy"
-  depends_on         = [aws_appautoscaling_target.scale_target[0]]
-  service_namespace  = "ecs"
-  resource_id        = "service/${var.cluster}/${var.service}"
-  scalable_dimension = "ecs:service:DesiredCount"
-  step_scaling_policy_configuration {
-    adjustment_type         = "ChangeInCapacity"
-    cooldown                = var.scaling.scale_up.memory.cooldown
-    metric_aggregation_type = "Average"
-    step_adjustment {
-      metric_interval_lower_bound = 0
-      scaling_adjustment          = 1
-    }
-  }
-}
-
-resource "aws_appautoscaling_policy" "memory_scale_down_policy" {
-  count              = var.scaling.enabled ? 1 : 0
-  name               = "${var.service}-memory-scale-down-policy"
-  depends_on         = [aws_appautoscaling_target.scale_target[0]]
-  service_namespace  = "ecs"
-  resource_id        = "service/${var.cluster}/${var.service}"
-  scalable_dimension = "ecs:service:DesiredCount"
-  step_scaling_policy_configuration {
-    adjustment_type         = "ChangeInCapacity"
-    cooldown                = var.scaling.scale_down.memory.cooldown
-    metric_aggregation_type = "Average"
-    step_adjustment {
-      metric_interval_upper_bound = 0
-      scaling_adjustment          = -1
-    }
+    target_value       = var.scaling.memory.threshold
+    scale_in_cooldown  = var.scaling.scale_down.memory.cooldown
+    scale_out_cooldown = var.scaling.scale_up.memory.cooldown
   }
 }
 
