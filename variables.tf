03ced9a78b5fdda71419cbe2b603c098d7660049
Calxus <calxus@protonmail.ch>
2022-10-25 13:08:24 +0100

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -74,12 +74,14 @@ variable "create_bucket" {
 variable "sidecar" {
   description = "Whether to enable service discovery of tasks"
   type = object({
+    name.       = string
     image       = string
     cpu         = number
     memory      = number
     environment = list(map(string))
   })
   default = {
+    name        = ""
     image       = ""
     cpu         = 0
     memory      = 0
